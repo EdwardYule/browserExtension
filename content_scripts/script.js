68f8e1e7329fefaec7c580a55773f756603103a2
Edward <nowhyman@hotmail.com>
2024-08-01 00:35:47 +0800

    ✨ feat: 对首个回答生效

--- content_scripts/script.js
+++ content_scripts/script.js
@@ -1,1 +1,15 @@
-console.log('ddd')
+if(window.location.pathname.includes('question')){
+  window.addEventListener('load', () => {
+    const listItems = Array.from(document.querySelectorAll('.AnswerItem'));
+    listItems.forEach(element => {
+      const head = element.querySelector('.ContentItem-meta');
+      console.dir(head)
+      const richText = element.querySelector('.RichText');
+      console.dir(richText)
+      const words = richText.innerText;
+      const span = document.createElement('span');
+      span.innerText = `共有${words.length}个字`;
+      head.appendChild(span);
+    });
+  })
+}
