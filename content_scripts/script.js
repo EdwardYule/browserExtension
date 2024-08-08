95d153cac7441e0c281751b07818850750f58653
Edward <787322353@qq.com>
2024-08-08 11:54:43 +0800

    ✨ feat: 滚动加载

--- content_scripts/script.js
+++ content_scripts/script.js
@@ -1,15 +1,32 @@
 if(window.location.pathname.includes('question')){
-  window.addEventListener('load', () => {
-    const listItems = Array.from(document.querySelectorAll('.AnswerItem'));
-    listItems.forEach(element => {
-      const head = element.querySelector('.ContentItem-meta');
-      console.dir(head)
-      const richText = element.querySelector('.RichText');
-      console.dir(richText)
-      const words = richText.innerText;
-      const span = document.createElement('span');
-      span.innerText = `共有${words.length}个字`;
-      head.appendChild(span);
+  const handleList = () => {
+    setTimeout(() => {
+      const listItems = Array.from(document.querySelectorAll('.AnswerItem'));
+      listItems.forEach(element => {
+        const head = element.querySelector('.ContentItem-meta');
+        const richText = element.querySelector('.RichText');
+        const headSpan = head.querySelector('.words');
+        if (!headSpan) {
+          const words = richText.innerText;
+          const span = document.createElement('span');
+          span.className = 'words';
+          span.style = `margin-top: 8px;`
+          span.innerText = `共有${words.length}个字`;
+          head.appendChild(span);
+        }
+      });
     });
+  }
+  window.addEventListener('load', () => {
+    handleList();
+    const element = document.querySelector('[role="list"]');
+    const observer = new MutationObserver((mutationsList, observer) => {
+      for (const mutation of mutationsList) {
+        if (mutation.type === 'childList') {
+          handleList();
+        }
+      }
+    });
+    observer.observe(element, { childList: true }); // 开始观察
   })
 }
