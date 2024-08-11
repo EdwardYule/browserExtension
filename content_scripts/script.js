f279c10f81886c7215ef2f3e90b9af24de253c62
Edward <nowhyman@hotmail.com>
2024-08-11 18:55:51 +0800

    ✨ feat: 过多则省略

--- content_scripts/script.js
+++ content_scripts/script.js
@@ -13,6 +13,20 @@ if(window.location.pathname.includes('question')){
           span.style = `margin-top: 8px;`
           span.innerText = `共有${words.length}个字`;
           head.appendChild(span);
+          if(words.length > 1000){
+            // 超过一千个字，就缩成高度500px，然后再展开
+            // 但其实这样并不太准确
+            richText.style = `height: 500px; overflow: hidden; display: block;`;
+            const expandDiv = document.createElement('div');
+            expandDiv.className = 'expandSpan';
+            expandDiv.textContent = '展开';
+            expandDiv.style = 'text-align: right; margin-top: 4px; cursor: pointer;'
+            expandDiv.addEventListener('click', () => {
+              richText.style = `height: auto; overflow: visible; display: block;`;
+              expandDiv.remove();
+            });
+            richText.parentNode.appendChild(expandDiv);
+          }
         }
       });
     });
